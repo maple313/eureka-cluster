0243174a776598423e619172ea5b993668f0cc15
MM <1285146536@qq.com>
2019-04-15 22:30:02 +0800

    springcloud学习（三） ————ribbon

--- eureka-consumer/src/main/java/com/qin/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/qin/eurekaconsumer/EurekaConsumerApplication.java
@@ -2,10 +2,12 @@ package com.qin.eurekaconsumer;
 
 import com.netflix.loadbalancer.AvailabilityFilteringRule;
 import com.netflix.loadbalancer.IRule;
+import com.qin.ribbon.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.qin.eurekaconsumer"})
+@RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MySelfRule.class)
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
