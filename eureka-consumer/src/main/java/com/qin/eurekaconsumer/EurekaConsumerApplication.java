cb3baf54715c8c98e9009b00d0a0503629a35196
wangzb <wangzb@hzcominfo.com>
2019-04-23 22:28:45 +0800

    hystrix学习

--- eureka-consumer/src/main/java/com/qin/eurekaconsumer/EurekaConsumerApplication.java
+++ eureka-consumer/src/main/java/com/qin/eurekaconsumer/EurekaConsumerApplication.java
@@ -5,6 +5,8 @@ import com.netflix.loadbalancer.IRule;
 import com.qin.ribbon.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
@@ -16,6 +18,8 @@ import org.springframework.web.client.RestTemplate;
 @EnableEurekaClient
 @EnableFeignClients(basePackages = {"com.qin.eurekaconsumer"})
 @RibbonClient(name="MICROSERVICECLOUD-DEPT",configuration= MySelfRule.class)
+@EnableDiscoveryClient //服务发现
+@EnableCircuitBreaker//对hystrixR熔断机制的支持
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
