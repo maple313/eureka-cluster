b146f3ebda811177230707318c203b95e46b83f9
wangzb <wangzb@hzcominfo.com>
2019-04-25 21:48:44 +0800

    hystrix-dashboard

--- eureka-comsumer-hystrix-dashboard/src/main/java/com/qin/eurekacomsumerhystrixdashboard/EurekaComsumerHystrixDashboardApplication.java
+++ eureka-comsumer-hystrix-dashboard/src/main/java/com/qin/eurekacomsumerhystrixdashboard/EurekaComsumerHystrixDashboardApplication.java
@@ -10,6 +10,7 @@ public class EurekaComsumerHystrixDashboardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaComsumerHystrixDashboardApplication.class, args);
+
     }
 
 }
