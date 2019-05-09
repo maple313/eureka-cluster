0c21ca05e918f8f912ec515f58f89a12b0b426fe
MM <1285146536@qq.com>
2019-05-09 23:01:13 +0800

    时间格式化——————————yyyy/MM/dd 'T' HH:mm:ss.SSSZ

--- eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
+++ eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
@@ -2,6 +2,7 @@ package com.qin.eurekaconsumer.common.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * @author WZB
@@ -103,7 +104,7 @@ public class TimeUtilByWzb {
 
     /**
      * 6
-     * @description 判断时间格式: yyyy/MM/dd HH:mm:ss  hh为12小时制
+     * @description 判断时间格式: yyyy/MM/dd hh:mm:ss  hh为12小时制
      */
     public static boolean isYyyyMMddhhmmssSlashType(String date){
         SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd hh:mm:ss");
@@ -119,9 +120,32 @@ public class TimeUtilByWzb {
         return true;
     }
 
+    /**
+     * 6
+     * @description 判断时间格式: yyyy/MM/dd 'T' HH:mm:ss.Z  HH为24小时制
+     */
+    public static boolean isYyyyMMddHHmmssStandard(String date){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd 'T' HH:mm:ss.SSSZ");
+
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+            format.parse(date);
+            System.out.println(format.parse(date));
+            System.out.println(format.format(format.parse(date)));
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
 
-        boolean flag = isYyyyMMddHHmmssSlashType("2018/2/27 15:05:06");
+        boolean flag = isYyyyMMddHHmmssStandard("2018/2/27 T 15:05:06.008+0800");
         System.out.println(flag);
+
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd 'T' HH:mm:ss.SSSZ");
+        Date date=new Date();
+        System.out.println(format.format(date));
     }
 }
