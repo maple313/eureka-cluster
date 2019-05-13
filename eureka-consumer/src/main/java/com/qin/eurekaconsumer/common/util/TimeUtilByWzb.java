eb94fa1dabd9d7933763f0881ac04fabca688e11
MM <1285146536@qq.com>
2019-05-13 23:45:22 +0800

    时间工具类

--- eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
+++ eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
@@ -2,7 +2,10 @@ package com.qin.eurekaconsumer.common.util;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 /**
  * @author WZB
@@ -139,13 +142,85 @@ public class TimeUtilByWzb {
         return true;
     }
 
+    /**
+     * 7
+     * @description 判断两个日期的大小: yyyy-MM-dd
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     */
+    public static boolean isInputTrueByMode1(String beginTime,String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            if (beginDate.getTime()>=endDate.getTime()){
+                return false;
+            }
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 8
+     * @description 判断两个日期的大小: yyyy-MM-dd HH:mm:ss
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     */
+    public static boolean isInputTrueByMode2(String beginTime,String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            if (beginDate.getTime()>=endDate.getTime()){
+                return false;
+            }
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 9
+     * @description 获取时间段内的日期（每一天）: yyyy-MM-dd
+     */
+    public static List<Date> getDateList(String beginTime, String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        List<Date> dateList=new ArrayList<>();
+        Calendar calendar=Calendar.getInstance();
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            calendar.setTime(beginDate);
+            while (beginDate.getTime()<=endDate.getTime()){
+                dateList.add(calendar.getTime());
+                calendar.add(Calendar.DATE, 1);
+                beginDate = calendar.getTime();
+            }
+
+
+        } catch (ParseException e) {
+            return dateList;
+        }
+        return dateList;
+    }
+
     public static void main(String[] args) {
 
-        boolean flag = isYyyyMMddHHmmssStandard("2018/2/27 T 15:05:06.008+0800");
-        System.out.println(flag);
-
-        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd 'T' HH:mm:ss.SSSZ");
-        Date date=new Date();
-        System.out.println(format.format(date));
+        List<Date> list=getDateList("2019-05-01","2019-05-13");
+        System.out.println(list);
     }
 }
