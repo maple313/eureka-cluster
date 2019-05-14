634dd4f755e695f27755cce3410de39933c9c7ef
MM <1285146536@qq.com>
2019-05-14 21:42:36 +0800

    时间工具类

--- eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
+++ eureka-consumer/src/main/java/com/qin/eurekaconsumer/common/util/TimeUtilByWzb.java
@@ -125,7 +125,7 @@ public class TimeUtilByWzb {
 
     /**
      * 6
-     * @description 判断时间格式: yyyy/MM/dd 'T' HH:mm:ss.Z  HH为24小时制
+     * @description 判断时间格式: yyyy/MM/dd 'T' HH:mm:ssZ  HH为24小时制
      */
     public static boolean isYyyyMMddHHmmssStandard(String date){
         SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd 'T' HH:mm:ss.SSSZ");
@@ -192,7 +192,34 @@ public class TimeUtilByWzb {
 
     /**
      * 9
+     * @description 判断两个日期的大小: yyyy/MM/dd 'T' HH:mm:ssZ  HH为24小时制
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     */
+    public static boolean isInputTrueByMode3(String beginTime,String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy/MM/dd 'T' HH:mm:ss.SSSZ");
+
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            if (beginDate.getTime()>=endDate.getTime()){
+                return false;
+            }
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * 10
      * @description 获取时间段内的日期（每一天）: yyyy-MM-dd
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     * @return List<Date> 开始时间到结束时间的日期
      */
     public static List<Date> getDateList(String beginTime, String endTime){
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -211,6 +238,108 @@ public class TimeUtilByWzb {
                 beginDate = calendar.getTime();
             }
 
+        } catch (ParseException e) {
+            return dateList;
+        }
+        return dateList;
+    }
+
+    /**
+     * 11
+     * @description 获取时间段内的日期字符串（每一天）: yyyy-MM-dd
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     * @return List<String> 开始时间到结束时间的日期字符串
+     */
+    public static List<String> getStringDateList(String beginTime, String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+
+        List<String> dateList=new ArrayList<>();
+        Calendar calendar=Calendar.getInstance();
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            calendar.setTime(beginDate);
+            while (beginDate.getTime()<=endDate.getTime()){
+                dateList.add(format.format(calendar.getTime()));
+                calendar.add(Calendar.DATE, 1);
+                beginDate = calendar.getTime();
+            }
+
+        } catch (ParseException e) {
+            return dateList;
+        }
+        return dateList;
+    }
+
+    /**
+     * 12
+     * @description 获取时间段内的日期（每一天）: yyyy-MM-dd HH:mm:ss
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     * @return List<Date> 开始时间到结束时间的日期
+     */
+    public static List<Date> getDateListMode1(String beginTime, String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
+
+        List<Date> dateList=new ArrayList<>();
+        Calendar calendar=Calendar.getInstance();
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            String beginDate2=format2.format(beginDate)+" 00:00:00";
+            String endDate2=format2.format(endDate)+" 00:00:00";;
+            beginDate= format.parse(beginDate2);
+            endDate=format.parse(endDate2);
+            calendar.setTime(beginDate);
+            while (beginDate.getTime()<=endDate.getTime()){
+                dateList.add(calendar.getTime());
+                calendar.add(Calendar.DATE, 1);
+                beginDate = calendar.getTime();
+            }
+
+        } catch (ParseException e) {
+            return dateList;
+        }
+        return dateList;
+    }
+
+    /**
+     * 13
+     * @description 获取时间段内的日期字符串（每一天）: yyyy-MM-dd HH:mm:ss
+     * @param beginTime 开始时间
+     * @param endTime 结束时间
+     * @return List<String> 开始时间到结束时间的日期字符串
+     */
+    public static List<String> getStringDateListMode1(String beginTime, String endTime){
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat format2 = new SimpleDateFormat("yyyy-MM-dd");
+
+        List<String> dateList=new ArrayList<>();
+        Calendar calendar=Calendar.getInstance();
+        try {
+            // 设置lenient为false. 否则SimpleDateFormat会比较宽松地验证日期，比如2007/02/29会被接受，并转换成2007/03/01
+            format.setLenient(false);
+
+            Date beginDate= format.parse(beginTime);
+            Date endDate=format.parse(endTime);
+            String beginDate2=format2.format(beginDate)+" 00:00:00";
+            String endDate2=format2.format(endDate)+" 00:00:00";;
+            beginDate= format.parse(beginDate2);
+            endDate=format.parse(endDate2);
+            calendar.setTime(beginDate);
+            while (beginDate.getTime()<=endDate.getTime()){
+                dateList.add(format.format(calendar.getTime()));
+                calendar.add(Calendar.DATE, 1);
+                beginDate = calendar.getTime();
+            }
 
         } catch (ParseException e) {
             return dateList;
@@ -220,7 +349,7 @@ public class TimeUtilByWzb {
 
     public static void main(String[] args) {
 
-        List<Date> list=getDateList("2019-05-01","2019-05-13");
+        List<String> list=getStringDateListMode1("2019-05-01 13:40:30","2019-05-13 15:36:50");
         System.out.println(list);
     }
 }
