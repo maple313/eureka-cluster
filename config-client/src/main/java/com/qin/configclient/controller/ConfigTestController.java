5c38ad5bc029a0cbb5c9096763b3fbcf4ac5d652
MM <1285146536@qq.com>
2019-05-14 22:31:07 +0800

    springcloud学习（五） ————config

--- config-client/src/main/java/com/qin/configclient/controller/ConfigTestController.java
+++ config-client/src/main/java/com/qin/configclient/controller/ConfigTestController.java
@@ -1,5 +1,6 @@
 package com.qin.configclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class ConfigTestController {
 
+    @Value("${liuxin.name:''}")
+    private String name;
+
     @RequestMapping("/config")
     public String getConfig(){
-        return "";
+        return name;
     }
 }
